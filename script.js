7f6a1787a3921c3c6cd18ba421b2c5de9301c3d1
Rasheed Ibraheem <arib1052001@gmail.com>
2020-09-03 15:50:47 -0700

    add app.js file

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const openIcon = document.getElementById("open-icon");
 const navbar = document.querySelector(".navbar");
 const heroImg = document.getElementById("hero-image");
 
+window.onresize = () => {
+    if (window.innerWidth >= 750) {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "none";
+        navbar.style.display = "block";
+    } else {
+        navbar.style.display = "none";
+        openIcon.style.display = "inline-block";
+    }
+}
+
+
 openIcon.onclick = () => {
     navbar.style.display = "block";
     openIcon.style.display = "none";
